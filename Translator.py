de046e0592bb62ffa5f3b6e4445a2ec88ecbafb1
Jan Adrian Fetizanan <applicatnccnt@gmail.com>
2020-03-01 00:39:55 +0800

    Renamed a varible

--- Translator.py
+++ Translator.py
@@ -4,11 +4,11 @@ skill_list = [
     ["BOS", "SE", "TAE", "AA", "BFT", "SI", "DE", "VL"],
     ["MFCSA", "FP", "IFHE", "SS", "AFT", "MAAF", "RPF", "CE"]
 ]
-# bits_arr = ["10000001", "00100001", "00010001", "00000011"]
-# result = [skill_list[s][i] for s, bits in enumerate(bits_arr) for i, v in enumerate(bits) if v == "1"]
+# bits_list = ["10000001", "00100001", "00010001", "00000011"]
+# result = [skill_list[s][i] for s, bits in enumerate(bits_list) for i, v in enumerate(bits) if v == "1"]
 # print(result)
 
-in_str = "PT,PM,AR,LS,SI,CE,TA,AFT"
+in_str = "PT,EM,HA,SI,BOS,CE,FP"
 in_str = list(set("".join(in_str.split()).split(',')))
 tiered_skills = [int(''.join(["1" if x in [skill_group.index(c) for c in in_str if c in skill_group] else "0" for x
                               in range(8)]), 2) for skill_group in skill_list]
